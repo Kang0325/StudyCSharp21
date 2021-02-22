d37e099554a25c4b3863ef23432b6060b1cf6c4c
Kang0325 <noworry1@naver.com>
2021-02-22 17:27:55 +0900

    2월 22일 수업 끝

---
+++ chap04/Chap04App/IncDecTestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace IncDecTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("증감연산자 테스트");
+
+            int result = 26;
+            Console.WriteLine($"현재 숫자는 {result}");
+            result += 3;
+            Console.WriteLine($"현재 숫자는 {result}");
+            result -= 10;
+            Console.WriteLine($"현재 숫자는 {result}");
+
+            Console.WriteLine($"전치 증감 현재 숫자는 {++result}");
+
+            Console.WriteLine($"후치 증감 현재 숫자는 {result++}");
+
+            Console.WriteLine($"후치 증감 후 현재 숫자는 {result}");
+
+            Console.WriteLine($"현재 숫자는 {result--}");
+            Console.WriteLine($"현재 숫자는 {--result}");
+        }
+    }
+}
