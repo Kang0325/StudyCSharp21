0d18944809615cf26c6538c19f80318111b2441d
Kang0325 <noworry1@naver.com>
2021-02-22 17:05:48 +0900

    3장 완료

---
+++ chap03/Chap03App/VarTestApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VarTestApp
+{
+    class Program
+    {
+        static float i = 100.5f;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"값은 {i}입니다.");
+        }
+    }
+}
