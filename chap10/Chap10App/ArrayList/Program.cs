a782e031ec339a7e0e1bcb4fd5d240397436cf0f
Kang0325 <noworry1@naver.com>
2021-03-02 10:37:19 +0900

    10장 커밋

---
+++ chap10/Chap10App/ArrayList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList array = new ArrayList(new[] { 80, 74, 81, 90, 34 });
+
+            var loc = array.IndexOf(81);
+            array.Insert(loc, 50);
+
+            Console.WriteLine("81 위치에 50 추가");
+            foreach (var item in array)
+            {
+                Console.WriteLine($" {item}");
+            }
+            Console.WriteLine("90 삭제");
+            loc = array.IndexOf(90);
+            array.RemoveAt(loc); // 90삭제
+
+            foreach (var item in array)
+            {
+                Console.WriteLine($" {item}");
+            }
+            
+            Console.WriteLine("정렬");
+            array.Sort();
+            foreach (var item in array)
+            {
+                Console.WriteLine($" {item}");
+            }
+        }
+    }
+}
---
+++ chap10/Chap10App/HashTableApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HashTable 예제");
+
+            Hashtable ht = new Hashtable();
+            ht["일"] = "One";
+            ht["이"] = "Two";
+            ht["삼"] = "Three";
+            ht["사"] = "Four";
+
+            Console.WriteLine(ht["일"]); // One
+            Console.WriteLine(ht["이"]); // Two
+
+            foreach (DictionaryEntry item in ht)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+        }
+    }
+}
