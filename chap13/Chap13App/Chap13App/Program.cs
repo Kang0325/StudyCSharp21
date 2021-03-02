00dc2b600a833e6f18eb0bae5431ad2b774cc835
Kang0325 <noworry1@naver.com>
2021-03-02 17:25:42 +0900

    13장 대리자

--- chap13/Chap13App/Chap13App/Program.cs
+++ chap13/Chap13App/Chap13App/Program.cs
@@ -2,11 +2,32 @@
 
 namespace Chap13App
 {
+    delegate int MyDelegate(int a, int b); //대리자. 대신 호출한다.
+
     class Program
     {
+        class Calculator
+        {
+            public int Plus(int a, int b)
+            {
+                return a + b;
+            }
+
+            public int Minus(int a, int b)
+            {
+                return a - b;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Calculator calc = new Calculator();
+            MyDelegate Callback;
+
+            Callback = new MyDelegate(calc.Plus);
+            Console.WriteLine($"result = {Callback(3, 4)}");
+
+            Callback = new MyDelegate(calc.Minus);
+            Console.WriteLine($"result = {Callback(4, 3)}");
         }
     }
 }
