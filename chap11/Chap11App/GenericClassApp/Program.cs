7c7298ea8d8fca651ec7700785fc00c22a18a8d8
Kang0325 <noworry1@naver.com>
2021-03-02 12:30:13 +0900

    11장 커밋

---
+++ chap11/Chap11App/GenericClassApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GenericClassApp
+{
+    class Array_Generic<T>
+    {
+        private T[] array;
+
+        public Array_Generic()
+        {
+            array = new T[10];
+        }
+
+        public T GetElement(int index) { return array[index]; }
+
+        public int Length
+        {
+            get { return array.Length; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array_Generic<int> array = new Array_Generic<int>();
+            Array_Generic<string> strArr = new Array_Generic<string>();
+        }
+    }
+}
